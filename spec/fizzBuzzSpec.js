74e885c4b82127eb2bf8ad433f2f23a61c7bb537
Ahmed Marzook <m4marzook@gmail.com>
2018-11-05 16:04:54 +0000

    my bad this si the actual test

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,3 +1,11 @@
-describe('', function() {
-  
+describe('Javabuzz', function() {
+
+  var javabuzz;
+
+  describe('knows when a number is', function() {
+    it('divisable by 3', function() {
+      javabuzz = new Javabuzz();
+      expect(isDivisibleByThree(3)).toBe(true);
+    });
+  });
 });
