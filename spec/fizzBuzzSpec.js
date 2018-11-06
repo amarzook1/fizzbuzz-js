c24c2dd42f9866416f0ff0a33b94fd7d1edbe00f
Ahmed Marzook <m4marzook@gmail.com>
2018-11-06 09:47:16 +0000

    Divisiable by 5 test

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -2,16 +2,31 @@ describe('Javabuzz', function() {
 
   var javabuzz;
 
+  beforeEach(function() {
+    javabuzz = new Javabuzz
+  });
+
   describe('knows when a number is', function() {
     it('divisable by 3', function() {
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByThree(3)).toBe(true);
     });
   });
+
   describe('Knows when a number is NOT', function() {
     it('divisable by 3', function() {
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByThree(1)).toBe(false);
+    });
+  });
+
+  describe('knows when a number is', function() {
+    it('divisable by 5', function() {
+      expect(javabuzz.isDivisibleByFive(5).toBe(true));
+    });
+  });
+
+  describe('Knows when a number is NOT', function() {
+    it('divisable by 5', function() {
+      expect(javabuzz.isDivisibleByFive(1)).toBe(false);
     });
   });
 });
